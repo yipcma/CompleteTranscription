f3573b162c1ccd73aa8eccae3a346a76782ab02a
Andrew Yip <yipcma@gmail.com>
2017-07-08 16:22:03 +0300

    add symlinks script; remove paths in main script

---
+++ symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s ~/repos/CompleteTranscription/modules/melisma/key/key ~/bin/.
+ln -s ~/repos/CompleteTranscription/modules/melisma/harmony/harmony ~/bin/.
+ln -s ~/repos/CompleteTranscription/modules/melisma/mftext-release-07/mftext ~/bin/.
+ln -s ~/repos/CompleteTranscription/modules/melisma2/polyph ~/bin/.
